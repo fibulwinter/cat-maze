809665961914fc3b90ca853cbba1238a537a55b5
Yegor <ymenovchshikov@gmail.com>
2016-12-05 11:40:13 +1100

    organization

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,11 @@
+library catRules;
+
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
+
+
+part 'cat.dart';
+
 
 ResourceManager resourceManager = new ResourceManager();
 Ground ground;
@@ -129,72 +135,5 @@ class Ground extends DisplayObjectContainer {
 
   removeStar() {
     removeChild(star);
-  }
-}
-
-class Cat extends Bitmap implements Animatable {
-  World world;
-  var mapX = 0;
-  var mapY = 0;
-  var targetX = 0;
-  var targetY = 0;
-
-  Cat(BitmapData bitmapData, this.world) : super(bitmapData) {
-    pivotX = width / 2;
-    pivotY = height / 2;
-    x = pivotX;
-    targetX = x;
-    y = pivotY;
-    targetY = y;
-  }
-
-  @override
-  bool advanceTime(num time) {
-    var z = 0.05;
-    x = (1 - z) * x + z * targetX;
-    y = (1 - z) * y + z * targetY;
-    return true;
-  }
-
-  tryWalk(int dx, int dy) {
-    if (world.canWalk(mapX + dx, mapY + dy)) {
-      mapX = mapX + dx;
-      mapY = mapY + dy;
-      targetX = targetX + dx * width;
-      targetY = targetY + dy * 80;
-      if (world.hasStar(mapX, mapY)) {
-        world.removeStar(mapX, mapY);
-        ground.removeStar();
-        purr();
-      }
-    } else {
-      meow();
-    }
-  }
-
-  walkRight() {
-    scaleX = 1;
-    tryWalk(1, 0);
-  }
-
-  walkLeft() {
-    scaleX = -1;
-    tryWalk(-1, 0);
-  }
-
-  walkUp() {
-    tryWalk(0, -1);
-  }
-
-  walkDown() {
-    tryWalk(0, 1);
-  }
-
-  meow() {
-    resourceManager.getSound('meow').play();
-  }
-
-  purr() {
-    resourceManager.getSound('purr').play();
   }
 }
